85156b74912c78c34de5f5a31167103cb66ae18d
Aleh Fiodarau <fedorov.oleg@gmail.com>
2026-08-05 19:55:30 +0800

    Add option to run github actions manually

---
+++ bbticker/Service/ApiKeyExpiration/Tests/ApiKeyExpirationTests.swift
@@ -0,0 +1,16 @@
+//
+//  ApiKeyExpirationTests.swift
+//  bbtickerTests
+//
+//  Created by Aleh Fiodarau on 8/5/26.
+//
+
+import Testing
+
+struct ApiKeyExpirationTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
