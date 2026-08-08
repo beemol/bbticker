483d8cab26fafaf5e4f508c37dc6b4b154e69893
Aleh Fiodarau <fedorov.oleg@gmail.com>
2026-08-08 14:18:37 +0800

    Fix IAP tests enviroments settings

--- bbticker/IAP/IAPManager.swift
+++ bbticker/IAP/IAPManager.swift
@@ -36,6 +36,7 @@ actor IAPManager: IAPManagerProtocol {
 
     func purchasePro() async throws -> Bool {
         guard let product = try await Product.products(for: [proProductId]).first else {
+            print("IAP: product not found for \(proProductId)")
             return false
         }
 
